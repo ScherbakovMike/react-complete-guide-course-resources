d5bc6fde03033dfd5e41334244c3cb59104aef91
ScherbakovMike <scherbakov.mike@gmail.com>
2024-02-11 06:33:20 +0300

    Creating components

--- code/03 React Essentials/02-creating-using-first-component/src/App.jsx
+++ code/03 React Essentials/02-creating-using-first-component/src/App.jsx
@@ -1,25 +1,53 @@
+import reactImg from './assets/react-core-concepts.png'
+import {CORE_CONCEPTS} from "./data.js";
+
 function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
+    return (
+        <header>
+            <img src={reactImg} alt="Stylized atom"/>
+            <h1>React Essentials</h1>
+            <p>
+                Fundamental React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    );
+}
+
+function CoreConcept({image, title, description}) {
+    return <li>
+        <img src={image} alt=""/>
+        <h3>{title}</h3>
+        <p>{description}</p>
+    </li>
 }
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            {...CORE_CONCEPTS[0]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[1]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[2]}
+                        />
+                        <CoreConcept
+                            {...CORE_CONCEPTS[3]}
+                        />
+                    </ul>
+
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
