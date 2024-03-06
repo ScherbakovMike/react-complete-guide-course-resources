bf8a01f787fdddb47995cc13dc1e4aebc8fb8934
ScherbakovMike <scherbakov.mike@gmail.com>
2024-03-06 09:50:16 +0300

    Game Over screen has been implemented.

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,31 +1,22 @@
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-export default function GameBoard({onSelectSquare, turns}) {
-    let gameBoard = initialGameBoard;
-    turns.forEach(turn => {
-        gameBoard[turn.square.row][turn.square.col] = turn.player;
-        return gameBoard;
-    });
-
-    return <ol id={"game-board"}>
-        {
-            gameBoard.map(
-                (row, rowIndex) =>
-                    (<li key={rowIndex}>
-                        <ol>
-                            {row.map((playerSymbol, colIndex) =>
-                                (<li key={colIndex}>
-                                    <button
-                                        onClick={() => onSelectSquare(rowIndex, colIndex)}>
-                                        {playerSymbol}
-                                    </button>
-                                </li>))}
-                        </ol>
-                    </li>)
-            )
-        }
-    </ol>
+export default function GameBoard({ onSelectSquare, board }) {
+    return (
+        <ol id="game-board">
+            {board.map((row, rowIndex) => (
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            <li key={colIndex}>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}
+                                >
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
 }
--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,29 +1,34 @@
-import {useState} from "react";
+import { useState } from 'react';
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({ initialName, symbol, isActive }) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(editing => !editing);
+        setIsEditing((editing) => !editing);
     }
 
-    function handleChange(e) {
-        setPlayerName(e.target.value);
+    function handleChange(event) {
+        setPlayerName(event.target.value);
     }
 
-    let editablePlayerName = <span className={"player-name"}>{playerName}</span>;
-    let btnCaption = 'Edit';
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    // let btnCaption = 'Edit';
 
     if (isEditing) {
-        editablePlayerName = <input type={"text"} required value={playerName} onChange={handleChange}/>
-        btnCaption = 'Save';
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} />
+        );
+        // btnCaption = 'Save';
     }
-    return (<li className={isActive ? 'active' : undefined}>
-                    <span className={"player"}>
-                        {editablePlayerName}
-                        <span className={"player-symbol"}>{symbol}</span>
-                    </span>
-        <button onClick={handleEditClick}>{btnCaption}</button>
-    </li>)
+
+    return (
+        <li className={isActive ? 'active' : undefined}>
+      <span className="player">
+        {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+      </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    );
 }
